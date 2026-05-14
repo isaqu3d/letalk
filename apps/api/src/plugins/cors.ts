76c840db14ebeffedece027070755d8b8d1224a1
Isaque de Sousa <isaque.error404@gmail.com>
2026-05-14 12:08:57 -0300

    refactor: simplify corsPlugin definition by using fastify-plugin

--- apps/api/src/plugins/cors.ts
+++ apps/api/src/plugins/cors.ts
@@ -1,13 +1,13 @@
 import fastifyCors from "@fastify/cors";
-import type { FastifyPluginAsync } from "fastify";
+import fp from "fastify-plugin";
 import { env } from "../config/env";
 
 const ALLOWED_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"];
 
-export const corsPlugin: FastifyPluginAsync = async (app) => {
+export const corsPlugin = fp(async (app) => {
   await app.register(fastifyCors, {
     origin: env.CORS_ORIGIN,
     methods: ALLOWED_METHODS,
     credentials: true,
   });
-};
+});
