14003ab6892e4c67bd94544abc8f2ab567311806
Isaque de Sousa <isaque.error404@gmail.com>
2026-05-14 11:40:41 -0300

    feat: create index file to export CnaeSegment and EmployeeRange types and functions

---
+++ packages/shared/src/domain/index.ts
@@ -0,0 +1,8 @@
+export {
+  extractCnaeDivision,
+  segmentFromCnae,
+} from "./cnae-segments";
+export type { CnaeSegment } from "./cnae-segments";
+
+export { estimateEmployeeRange } from "./employee-range";
+export type { EmployeeRange, EmployeeRangeInput } from "./employee-range";
